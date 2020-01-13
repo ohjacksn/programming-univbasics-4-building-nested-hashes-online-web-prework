2b864b00c869c17d63c2680ed7a5ad48aaf2e532
Jackson Mo <jacksonmo@me.com>
2020-01-13 18:01:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,12 @@ def monopoly_with_third_tier
   base_hash = {
     railroads: {
       pieces: 4,
-      rent_in_dollars: 10
+      rent_in_dollars: {
+        one_piece_owned:
+        two_pieces_owned:
+        three_pieces_owned:
+        four_pieces_owned:
+      }
     }
   }
 end
